80ffc320f7a03ec59548b0a1d1edfb3b98ad3663
Code1912 <code1912@163.com>
2017-07-30 16:15:08 +0800

    Initial commit

---
+++ src/ng-signature.js
@@ -0,0 +1,66 @@
+/**
+ * Created by Code1912 on 2017/7/30.
+ */
+(function (angular) {
+    angular.module('app').directive('ngSignature',function ($timeout) {
+        return {
+            restrict: 'A',
+            replace: true,
+            scope : {
+                ngSignature : '=',
+                onLoad:'&',
+                signatureData:'=',
+                sColor:'@',
+                sWidth:'@',
+                sHeight:'@',
+                sWidth:'@',
+               sBackgroundColor:'@',
+                sLineWidth:'@'
+            },
+            link : function(scope, element, attrs) {
+                var $sigdiv = $(element).jSignature(getSetting(scope));
+                $sigdiv.jSignature("reset") ;
+
+                $sigdiv.bind('change',function (e) {
+                    $timeout(function () {
+                        scope.signatureData=$sigdiv.jSignature('getData', 'default');
+                    })
+
+                });
+
+                if(scope.signatureData ){
+                    $sigdiv.jSignature("setData",  scope.signatureData)
+                }
+
+                scope.ngSignature = {
+                    reset:function () {
+                        $sigdiv.jSignature("reset")
+                    },
+                    getDefaultData:function () {
+                      return   $sigdiv.jSignature('getData', 'default')
+                    }
+                };
+                if(scope.onLoad){
+                    $timeout(scope.onLoad)
+                }
+            }
+        };
+    });
+
+    function getSetting(scope) {
+        var propMap={
+            'sWidth':'width',
+            'sHeight':'height',
+            'sColor':'color',
+            'sBackgroundColor':'background-color',
+            'sLineWidth':'lineWidth'};
+        var setting = {};
+
+         for(var key in propMap){
+             if(scope[key]){
+                 setting[propMap[key]]=scope[key];
+             }
+         }
+        return  setting;
+    }
+})(window.angular);
